e883222685198e567d99ca760d0d0059dd6157c7
Gloria Lartey <glorialarety15@gmail.com>
2026-05-05 09:57:31 +0000

    Nodemailer update 1

--- src/utils/sendmail.ts
+++ src/utils/sendmail.ts
@@ -3,12 +3,10 @@ import nodemailer from "nodemailer";
 export const sendMail = async (to: string, subject: string, html: string) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // TLS
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // MUST be Gmail App Password
+        pass: process.env.EMAIL_PASS,
       },
       connectionTimeout: 10000, // 10s timeout
       greetingTimeout: 10000,
